435d9da9dba4591633a272a29b7c3ea95e1a95f0
godcindy <cindygodoy@pursuit.org>
2023-12-10 12:54:24 -0500

    final

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Greeting({name, age}) {
+
+
+  return (
+    <div>
+      Hello, {name}!
+      <div>
+        Hello, {name}!</div>
+    </div>
+  );
+}
+
+export default App;
